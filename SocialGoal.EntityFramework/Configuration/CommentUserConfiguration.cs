b93a789b4858f35a7d4910609fa62420227bc042
xu <xujiesh0510@live.cn>
2016-06-27 20:16:22 +0800

    fffffff

---
+++ SocialGoal.EntityFramework/Configuration/CommentUserConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocialGoal.Models;
+
+namespace SocialGoal.Configuration
+{
+   public class CommentUserConfiguration:EntityTypeConfiguration<CommentUser>
+   {
+       public CommentUserConfiguration()
+       {
+           Property(s => s.CommentId).IsRequired();
+           Property(s => s.UserId).IsRequired();
+       }
+   }
+}
