72c4db704f8a11b270e9106d253e0a5fd16214f5
xu <xujiesh0510@live.cn>
2016-06-27 20:16:32 +0800

    fffff

--- SocialGoal.Core/Models/CommentUser.cs
+++ SocialGoal.Core/Models/CommentUser.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+ 
 
 namespace SocialGoal.Models
 {
     public class CommentUser
     {
+        public int CommentUserId { get; set; }
+        public int CommentId { get; set; }
+        public string UserId { get; set; }  
     }
 }
